d1270ec5b88723a9ef753948ae35834f56993fb5
Ubuntu <asbassan@gmail.com>
2021-02-07 16:14:41 +0000

    I am pushing all the files to my GItHub

---
+++ C_Program.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+printf(“Hello! I am C-Program. Thank you!”);
+
+return 0;
+
+}
+
+●	Execute vi HelloJava.java and add the below code in HelloJava.java file:
+
+class HelloJava {
+
+public static void main(String args[]){
+
+System.out.Println(“ I am your Java Program. Thank you! ”);
+
+}
+
+}
+
+
